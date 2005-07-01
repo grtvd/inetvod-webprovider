b9fe72ee15929954da9b7a880f9db2dd80684d85
grtvd <grtvd@users.noreply.github.com>
2005-07-01 14:02:10 -0400

    webprovider: Update StatusCode. Added new IDs, updated convertFromInt().

--- src/com/inetvod/common/core/StatusCode.java
+++ src/com/inetvod/common/core/StatusCode.java
@@ -4,21 +4,41 @@
  */
 package com.inetvod.common.core;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StatusCode
 {
 	public static final StatusCode sc_Success = new StatusCode(0);
 
-	public static final StatusCode sc_InvalidUserID = new StatusCode(1000);
-//	public static final StatusCode sc_UserIDInUse = new StatusCode(1001);
-	public static final StatusCode sc_UserIDPasswordMismatch = new StatusCode(1002);
-	public static final StatusCode sc_InvalidProviderUserIDPassword = new StatusCode(1003);
+	public static final StatusCode sc_RequestNotSupported = new StatusCode(1);
+	public static final StatusCode sc_RequestMissingRequired = new StatusCode(2);
 
-	public static final StatusCode sc_AlreadyEnrolledAtProvider = new StatusCode(1004);
-	public static final StatusCode sc_NoAutoProviderEnrollment = new StatusCode(1005);
+	public static final StatusCode sc_InvalidMemberUserID = new StatusCode(1000);
+	public static final StatusCode sc_InvalidAdminUserID = new StatusCode(1001);
+	public static final StatusCode sc_UserIDInUse = new StatusCode(1002);
 
-	public static final StatusCode sc_ShowSearch_NeedCriteiia = new StatusCode(1020);
+	public static final StatusCode sc_ShowNoAccess = new StatusCode(1100);
+	public static final StatusCode sc_ShowLevelInsufficient = new StatusCode(1101);
+	public static final StatusCode sc_ShowNotRented = new StatusCode(1102);
+	public static final StatusCode sc_ShowRentExpired = new StatusCode(1103);
 
 	public static final StatusCode sc_GeneralError = new StatusCode(9999);
+
+	private static List<StatusCode> fAllValues = Arrays.asList(new StatusCode[]
+		{
+			sc_Success,
+			sc_RequestNotSupported,
+			sc_RequestMissingRequired,
+			sc_InvalidMemberUserID,
+			sc_InvalidAdminUserID,
+			sc_UserIDInUse,
+			sc_ShowNoAccess,
+			sc_ShowLevelInsufficient,
+			sc_ShowNotRented,
+			sc_ShowRentExpired,
+			sc_GeneralError
+		});
 
 	protected int fValue;
 
@@ -32,32 +52,18 @@ public class StatusCode
 		if(value == null)
 			return sc_Success;
 
-		int val = value.intValue();
+		for(StatusCode statusCode : fAllValues)
+			if(statusCode.fValue == value)
+				return statusCode;
 
-		if(val == sc_Success.fValue)
-			return sc_Success;
-		if(val == sc_InvalidUserID.fValue)
-			return sc_InvalidUserID;
-//		if(val == sc_UserIDInUse.fValue)
-//			return sc_UserIDInUse;
-		if(val == sc_UserIDPasswordMismatch.fValue)
-			return sc_UserIDPasswordMismatch;
-		if(val == sc_InvalidProviderUserIDPassword.fValue)
-			return sc_InvalidProviderUserIDPassword;
-		if(val == sc_AlreadyEnrolledAtProvider.fValue)
-			return sc_AlreadyEnrolledAtProvider;
-
-		if(val == sc_ShowSearch_NeedCriteiia.fValue)
-			return sc_ShowSearch_NeedCriteiia;
-		
 		return sc_GeneralError;
 	}
 
 	public static Integer convertToInt(StatusCode statusCode)
 	{
 		if(statusCode == null)
-			return new Integer(sc_Success.fValue);
+			return sc_Success.fValue;
 
-		return new Integer(statusCode.fValue);
+		return statusCode.fValue;
 	}
 }
