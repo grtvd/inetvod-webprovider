978a9cba420df46584bb8d298b076d4bac16a4f5
grtvd <grtvd@users.noreply.github.com>
2005-10-18 23:05:56 -0400

    webprovder: Added logic to load 1 of 4 data.xml files based on URL root app name.

--- src/com/inetvod/webprovider/WebProviderServlet.java
+++ src/com/inetvod/webprovider/WebProviderServlet.java
@@ -7,14 +7,16 @@ package com.inetvod.webprovider;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.inetvod.common.core.ServletFulfiller;
-import com.inetvod.provider.rqdata.DataManager;
+import com.inetvod.common.core.Logger;
 import com.inetvod.provider.request.WebProviderServletFulfiller;
+import com.inetvod.provider.rqdata.DataManager;
 import org.apache.log4j.xml.DOMConfigurator;
 
 public class WebProviderServlet extends HttpServlet
@@ -32,9 +34,11 @@ public class WebProviderServlet extends HttpServlet
 		}
 		catch(MalformedURLException e)
 		{
+			e.printStackTrace();
 		}
 
-		realPath = getServletContext().getRealPath("/data.xml");
+		//TODO: can remove, the DataManager is a helper object to simulate database access
+		realPath = getServletContext().getRealPath(determineDataFile());
 		file = new File(realPath);
 		try
 		{
@@ -42,7 +46,51 @@ public class WebProviderServlet extends HttpServlet
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+			Logger.logErr(this, "init", e);
+		}
+	}
+
+	//TODO: can remove, only needed to load the DataManager object
+	private String determineDataFile()
+	{
+		final String contextBegin = "provider_";
+		final String defaultDataFile = "/data.xml";
+
+		String contextRoot = determineContextRoot();
+
+		if(contextRoot != null)
+		{
+			if(contextRoot.startsWith(contextBegin))
+			{
+				String dataFile = String.format("/data_%s.xml", contextRoot.substring(contextBegin.length()));
+				return dataFile;
+			}
+		}
+
+		return defaultDataFile;
+	}
+
+	//TODO: can remove, only needed to load the DataManager object
+	private String determineContextRoot()
+	{
+		try
+		{
+			URI realURL = getServletContext().getResource("/data.xml").toURI();
+
+			String realPath = realURL.getPath();
+			if((realPath == null) || (realPath.length() == 0))
+				return null;
+
+			String[] parts = realPath.split("\\/");
+			if((parts == null) || (parts.length < 3))
+				return null;
+
+			return parts[2];
+		}
+		catch(Exception e)
+		{
+			Logger.logErr(this, "determineContextRoot", e);
+			return null;
 		}
 	}
 
