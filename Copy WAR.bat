d16f9b9dbbf792c5a5b821c21ef9205e00119e91
grtvd <grtvd@users.noreply.github.com>
2005-11-04 14:10:37 -0500

    webprovider: Cut version 2005-11-04.

---
+++ Copy WAR.bat
@@ -0,0 +1,9 @@
+del provider_excellentvideos.war
+del provider_internetvideos.war
+del provider_moviesmovies.war
+del provider_vodflicks.war
+
+copy provider.war provider_excellentvideos.war
+copy provider.war provider_internetvideos.war
+copy provider.war provider_moviesmovies.war
+copy provider.war provider_vodflicks.war
