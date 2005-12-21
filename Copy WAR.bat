a8b6115585215d470b5199fa37f90f7e3dc212af
grtvd <grtvd@users.noreply.github.com>
2005-12-20 22:09:08 -0500

    webprovider: Added Provider Rocketboom.

--- Copy WAR.bat
+++ Copy WAR.bat
@@ -2,10 +2,12 @@ del provider_excellentvideos.war
 del provider_internetvideos.war
 del provider_mlb.war
 del provider_moviesmovies.war
+del provider_rocketboom.war
 del provider_vodflicks.war
 
 copy provider.war provider_excellentvideos.war
 copy provider.war provider_internetvideos.war
 copy provider.war provider_mlb.war
 copy provider.war provider_moviesmovies.war
+copy provider.war provider_rocketboom.war
 copy provider.war provider_vodflicks.war
