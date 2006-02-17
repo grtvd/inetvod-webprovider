f5392a90fc6ceab1881a845bf2829f57c51e9e7b
grtvd <grtvd@users.noreply.github.com>
2006-02-16 22:52:25 -0500

    webprovider: Updated log4j to not use DailyFileLog, just FileLog instead. Updated to fetch log4j file based on context.

--- src/com/inetvod/webprovider/WebProviderServlet.java
+++ src/com/inetvod/webprovider/WebProviderServlet.java
@@ -8,16 +8,18 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.inetvod.common.core.ServletFulfiller;
+import org.apache.log4j.xml.DOMConfigurator;
+
 import com.inetvod.common.core.Logger;
+import com.inetvod.common.core.ServletFulfiller;
 import com.inetvod.provider.request.WebProviderServletFulfiller;
 import com.inetvod.provider.rqdata.DataManager;
-import org.apache.log4j.xml.DOMConfigurator;
 
 public class WebProviderServlet extends HttpServlet
 {
@@ -26,7 +28,8 @@ public class WebProviderServlet extends HttpServlet
 		String realPath;
 		File file;
 
-		realPath = getServletContext().getRealPath("/log4j.xml");
+		//TODO: can revert to: realPath = getServletContext().getRealPath("/log4j.xml");
+		realPath = getServletContext().getRealPath(determineDataFile("log4j", ".xml"));
 		file = new File(realPath);
 		try
 		{
@@ -38,7 +41,7 @@ public class WebProviderServlet extends HttpServlet
 		}
 
 		//TODO: can remove, the DataManager is a helper object to simulate database access
-		realPath = getServletContext().getRealPath(determineDataFile());
+		realPath = getServletContext().getRealPath(determineDataFile("data", ".xml"));
 		file = new File(realPath);
 		try
 		{
@@ -51,19 +54,19 @@ public class WebProviderServlet extends HttpServlet
 	}
 
 	//TODO: can remove, only needed to load the DataManager object
-	private String determineDataFile()
+	private String determineDataFile(String defaultDataFilePrefix, String defaultDataFileExt)
 	{
-		final String contextBegin = "provider_";
-		final String defaultDataFile = "/data.xml";
+		String contextBegin = "provider_";
+		String defaultDataFile = "/" + defaultDataFilePrefix + defaultDataFileExt;
 
-		String contextRoot = determineContextRoot();
+		String contextRoot = determineContextRoot(defaultDataFile);
 
 		if(contextRoot != null)
 		{
 			if(contextRoot.startsWith(contextBegin))
 			{
-				String dataFile = String.format("/data_%s.xml", contextRoot.substring(contextBegin.length()));
-				return dataFile;
+				return String.format("/" + defaultDataFilePrefix + "_%s" + defaultDataFileExt,
+					contextRoot.substring(contextBegin.length()));
 			}
 		}
 
@@ -71,11 +74,11 @@ public class WebProviderServlet extends HttpServlet
 	}
 
 	//TODO: can remove, only needed to load the DataManager object
-	private String determineContextRoot()
+	private String determineContextRoot(String checkFile)
 	{
 		try
 		{
-			URI realURL = getServletContext().getResource("/data.xml").toURI();
+			URI realURL = getServletContext().getResource(checkFile).toURI();
 
 			String realPath = realURL.getPath();
 			if((realPath == null) || (realPath.length() == 0))
