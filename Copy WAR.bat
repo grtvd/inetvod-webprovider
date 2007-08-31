b521561f3602774014da73323ce91b18482f8b0c
grtvd <grtvd@users.noreply.github.com>
2007-08-30 23:10:32 -0400

    webprovider: Cut version 2007-08-30.

--- Copy WAR.bat
+++ Copy WAR.bat
@@ -1,11 +1,5 @@
-del provider_excellentvideos.war
 del provider_internetvideos.war
-del provider_mlb.war
 del provider_moviesmovies.war
-del provider_vodflicks.war
 
-copy provider.war provider_excellentvideos.war
 copy provider.war provider_internetvideos.war
-copy provider.war provider_mlb.war
 copy provider.war provider_moviesmovies.war
-copy provider.war provider_vodflicks.war
